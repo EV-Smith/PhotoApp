8b7da3826a2c03190cf132da4c9245b9b135a7e2
EV-Smith <emilie.v.smi@gmail.com>
2020-03-31 20:19:01 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+const passport = require('passport');
+
+// DB Config
+const dbConnectionString = require('./config/keys').mongoURI;
+
+const expressApp = express();
+
+// Body parser middleware.. express is the one getting data. urlencoded - textbox might have some characters
+// extended - you can come up with your own encoding
+expressApp.use(bodyParser.urlencoded({extended: false}));
+expressApp.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(dbConnectionString)
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
+
+// Passport initialization
+expressApp.use(passport.initialize());
+require('./config/passport')(passport);
+
+expressApp.use('/api/users', users);
+expressApp.use('/api/profile', profile);
+expressApp.use('/api/posts', posts);
+
+// Configure port to run web app
+const port=8090;
+expressApp.listen(port, () => console.log(`Server running on port ${port}`));
